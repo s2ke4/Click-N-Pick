d1506a550ef491665463222a66a8cade140a95b0
Keshav Agarwal <keshavagarwal1710@gmail.com>
2021-03-11 11:07:08 +0530

    small changes

--- router/sellerDashboard.js
+++ router/sellerDashboard.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const router = express.Router();
-
+var multer  = require('multer')
+var upload = multer({ dest: '/uploads/' })
 // all the routes will came here
 
 //seller dashboard route
@@ -16,6 +17,12 @@ router.get("/addItem",(req,res)=>{
 //Orders route to view the orders received by the seller
 router.get("/orders",(req,res)=>{
     res.render("seller/orders");
+})
+
+//router for adding item
+router.post("/addItem",upload.array('photos', 12),(req,res)=>{
+    console.log(req.files[0])
+    res.send("HELLO")
 })
 
 module.exports = router;
