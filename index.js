e8c965082e178d95ffd025234467f6f54fce9f50
Darshan Hande <darshanhande11@gmail.com>
2021-03-20 00:07:54 +0530

    linked the backend of the homepage

--- index.js
+++ index.js
@@ -5,6 +5,8 @@ const conn = require("./connection");
 const cookieParser = require("cookie-parser");
 const {checkUser,ensureGuest,ensureBuyer,ensureSeller} = require("./middleware/authMiddleware");
 const methodOverride = require("method-override");
+const util = require('util');
+const db = util.promisify(conn.query).bind(conn);
 
 conn.connect((err)=>{
     if(err) throw err;
@@ -72,8 +74,20 @@ app.use(methodOverride('X-HTTP-Method-Override'));
 
 app.get("*",checkUser);
 
-app.get("/",ensureGuest,(req,res)=>{
-    res.render("home");
+app.get("/",ensureGuest,async(req,res)=>{
+    let queryTopItems = `(SELECT * from items WHERE items.category='Grocery' LIMIT 4) UNION ALL (SELECT * FROM items WHERE items.category='Electronics' LIMIT 4) UNION ALL (SELECT * FROM items WHERE items.category='Clothing' LIMIT 4)
+    UNION ALL (SELECT * FROM items WHERE items.category='Footwear' LIMIT 4) UNION ALL (SELECT * FROM items WHERE items.category='Stationary' LIMIT 4) UNION ALL (SELECT * FROM items WHERE items.category='Novels' LIMIT 4)
+    UNION ALL (SELECT * FROM items WHERE items.category='Luggage' LIMIT 4) UNION ALL (SELECT * FROM items WHERE items.category='Furniture' LIMIT 4) UNION ALL (SELECT * FROM items WHERE items.category='Cosmetics' LIMIT 4)
+    UNION ALL (SELECT * FROM items WHERE items.category='Health and Medicine' LIMIT 4) UNION ALL (SELECT * FROM items WHERE items.category='Games' LIMIT 4) UNION ALL (SELECT * FROM items WHERE items.category='Home and Kitchen' LIMIT 4)`;
+    let topItems = await db(queryTopItems);
+    let imgs = [];
+    for(let i=0;i<topItems.length;i++)
+    {
+        query = `SELECT imgPath FROM attachment WHERE attachment.item_id=${topItems[i].id} LIMIT 1`;
+        let attachResult = await db(query);
+        imgs.push(attachResult[0]);
+    }
+    res.render("home",{topItems: topItems,images: imgs});
 })
 
 //route for seller login and signup
