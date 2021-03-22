0e0f09f982a09afea903a4591b2ee8220c1e4870
Keshav Agarwal <keshavagarwal1710@gmail.com>
2021-03-22 11:02:03 +0530

    minor change

--- public/js/addCart.js
+++ public/js/addCart.js
@@ -8,13 +8,3 @@ const addToCart = async(itemId,userId)=>{
     request.open("POST","/buyer/addToCart",true);
     request.send(formData);
 }
-
-document.getElementsByClassName("arechaljabhai")[0].addEventListener("click",async(e)=>{
-    console.log("HELLLLLLO")
-    var formData = new FormData();
-    formData.append("id1","id");
-    formData.append("id2","id");
-    var request = new XMLHttpRequest();
-    request.open("POST","/buyer/arechaljabhai",true);
-    request.send(formData);
-})
