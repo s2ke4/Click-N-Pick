3b375d45b175218a037610d684fd89c20b10ed00
Keshav Agarwal <keshavagarwal1710@gmail.com>
2021-04-25 00:16:51 +0530

    made some small changes

--- connection.js
+++ connection.js
@@ -2,15 +2,45 @@ const mysql = require("mysql");
 const dotenv = require("dotenv")
 dotenv.config({path:"./.env"})
 
-const conn = mysql.createConnection({
+
+var conn;
+
+
+var db_config = {
     host:process.env.DATABASE_HOST,
     user:process.env.DATABASE_USER,
     password:process.env.DATABASE_PASSWORD,
     database:process.env.DATABASE
-})
+};
 
-conn.connect((err)=>{
-    if(err) throw err;
+function handleDisconnect() {
+  conn = mysql.createConnection(db_config); // Recreate the connection, since
+                                                  // the old one cannot be reused.
+
+  conn.connect(function(err) {              // The server is either down
+    if(err) {                                     // or restarting (takes a while sometimes).
+      console.log('error when connecting to db:', err);
+      setTimeout(handleDisconnect, 2000); // We introduce a delay before attempting to reconnect,
+    }else{
+        performOperation()
+    }                                     // to avoid a hot loop, and to allow our node script to
+  });                                     // process asynchronous requests in the meantime.
+                                          // If you're also serving http, display a 503 error.
+  conn.on('error', function(err) {
+    console.log('db error', err);
+    if(err.code === 'PROTOCOL_CONNECTION_LOST') { // Connection to the MySQL server is usually
+      handleDisconnect();                         // lost due to either server restart, or a
+    } else {                                      // connnection idle timeout (the wait_timeout
+      throw err;                                  // server variable configures this)
+    }
+  });
+}
+
+handleDisconnect();
+
+
+const performOperation = ()=>{
+    // if(err) throw err;
     // code for creating database
 
     // let query1 = `CREATE DATABASE ${process.env.DATABASE}`;
@@ -102,6 +132,6 @@ conn.connect((err)=>{
         }
         console.log("orderitem table created successfully");
     })
-})
+}
 
 module.exports = conn;
